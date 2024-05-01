eee4773aa6b18d4e4c73cd6408ff0422ae922681
kwonjeong <kwonjeong9470@gmail.com>
2024-05-01 15:11:04 +0900

    [Design] #1 - 미션 2 UI 구현

--- week2/src/main.jsx
+++ week2/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Prac1 from './practice/Prac1'
 import Prac2 from './practice/Prac2'
-import Misson1 from './misson/Misson1'
+import Misson1 from './misson1/Misson1'
+import Misson2 from './misson2/Misson2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Prac1 />
     <Prac2 /> */}
-    <Misson1 />
+    {/* <Misson1 /> */}
+    <Misson2 />
   </React.StrictMode>,
 )
