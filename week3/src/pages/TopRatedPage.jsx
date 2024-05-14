147180328fe5e85efe7d777d451a0cb7e0f3195e
kwonjeong <kwonjeong9470@gmail.com>
2024-05-15 01:56:04 +0900

    [Feat] #3 - TopRated 영화 API 연결

--- week3/src/pages/TopRatedPage.jsx
+++ week3/src/pages/TopRatedPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import Movie from '../components/Common/MovieList';
 import data from '../data/movieList.json';
@@ -19,8 +19,30 @@ const MoviesWrapper = styled.div`
   flex-wrap: wrap;
 `;
 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MmU2M2Q4Y2I5ODlmOWRmMWMwZDZiMjcwMjUzOWU1YSIsInN1YiI6IjY2NDM5MjM4YWFkOTI4NzA1MTY5NDY2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XWAil1wXLZkUHxXjfAA7-m9-sLbFTP8Iv9hHZlmOCL8',
+  },
+};
+
 const TopRatedPage = () => {
-  const movies = data.results;
+  const [movies, setMovies] = useState([]);
+
+  console.log(movies)
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-kr&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200',
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setMovies(response.results))
+      .catch((err) => console.error(err))
+  }, []);
+
   return (
     <Body>
       <MoviesWrapper>
