2f7a4dcad604e9d1c8a37a5c3af0a72b8d90482a
kwonjeong <kwonjeong9470@gmail.com>
2024-07-02 22:46:44 +0900

    [Feat] #12 - MainPage 반응형 설정

--- Movie/src/pages/MainPage.jsx
+++ Movie/src/pages/MainPage.jsx
@@ -1,3 +1,4 @@
+// src/pages/MainPage.jsx
 import styled from "styled-components";
 import Welcome from "../components/Main/Welcome";
 import SearchBox from "../components/Main/SearchBox";
@@ -21,8 +22,16 @@ const St = {
         display: flex;
         flex-direction: column;
         align-items: center;
-
         background: #000000;
         width: 100%;
+        padding: 20px;
+
+        @media (max-width: 768px) {
+            padding: 10px;
+        }
+
+        @media (max-width: 480px) {
+            padding: 5px;
+        }
     `
 };
